81d3b7e3be10fb46c4b13e2643bc816fb0ec558a
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-10-23 09:11:25 +0530

    appbar + bg

--- daybreakglimmer/lib/main.dart
+++ daybreakglimmer/lib/main.dart
@@ -2,20 +2,39 @@ import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const LogInSignIn()); 
+void main() => runApp(EarthSaviours());
+
+class EarthSaviours extends StatelessWidget {
+  const EarthSaviours({Key? key}) : super(key: key);
+  //root application
+  @override 
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LogInSignIn(),
+    );
+  }
+}
+
 class LogInSignIn extends StatelessWidget {
   const LogInSignIn({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints.expand(),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage('https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
-          fit: BoxFit.cover)
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Helping Hands',
+        style: GoogleFonts.openSans(),),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(228, 205, 205, 0.75),
       ),
-      child: null
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
+            fit: BoxFit.cover)
+        ),
+        child: null,
+      ),
     );
   }
 }
