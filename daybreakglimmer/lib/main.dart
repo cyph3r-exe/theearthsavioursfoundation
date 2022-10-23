d7bf9c1bdfb7c28dd7e178b72bfe50e764a59e04
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-10-23 12:43:44 +0530

    minor changes

--- daybreakglimmer/lib/main.dart
+++ daybreakglimmer/lib/main.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 // ignore: unused_import
-import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(EarthSaviours());
 
@@ -22,18 +23,106 @@ class LogInSignIn extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Helping Hands',
-        style: GoogleFonts.openSans(),),
+        style: TextStyle(
+          fontFamily: 'Arial Rounded MT Bold'
+        )),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(228, 205, 205, 0.75),
+        foregroundColor: Colors.black,
+        shadowColor: Colors.black,
+        elevation: 20,
       ),
       body: Container(
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: NetworkImage('https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
+            image: AssetImage("lib/assets/AppBackground.png"),
             fit: BoxFit.cover)
         ),
-        child: null,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                constraints: BoxConstraints.tight(Size(300, 300)),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("lib/assets/logowithoutbg.png"))
+                ),
+              ),
+              PhysicalModel(color: Color.fromRGBO(228, 205, 205, 0.75),
+              shadowColor: Colors.black,
+              elevation: 20,
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                constraints: BoxConstraints.tight(Size(400,200)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 180,
+                      margin: EdgeInsets.all(2.5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color.fromRGBO(161, 91, 91, 0.75),
+                      ),
+                      child: ElevatedButton(onPressed: null, 
+                        child: Text('Login',
+                        style: TextStyle(
+                          fontFamily: 'Arial Rounded MT Bold',
+                        ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 180,
+                      margin: EdgeInsets.all(2.5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color.fromRGBO(161, 91, 91, 0.75),
+                      ),
+                      child: ElevatedButton(onPressed: null, 
+                        child: Text('Sign In',
+                        style: TextStyle(
+                          fontFamily: 'Arial Rounded MT Bold',
+                        ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    constraints: BoxConstraints.tight(
+                      Size(90, 90)
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      image: DecorationImage(
+                        image: AssetImage("lib/assets/tesflogo.png"),
+                        fit: BoxFit.cover
+                    ),
+                  ),
+                  ),
+                  Container(
+                    constraints: BoxConstraints.tight(
+                      Size(90, 90)
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      image: DecorationImage(
+                        image: AssetImage("lib/assets/zlwbg.png"),
+                        fit: BoxFit.cover)
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
       ),
     );
   }
