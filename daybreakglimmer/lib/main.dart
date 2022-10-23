439f0dcfc93b56054cede6e00a16b2d97d479235
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-10-23 13:26:32 +0530

    fix: image not loading

--- daybreakglimmer/lib/main.dart
+++ daybreakglimmer/lib/main.dart
@@ -36,7 +36,7 @@ class LogInSignIn extends StatelessWidget {
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("lib/assets/AppBackground.png"),
+            image: NetworkImage('https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
             fit: BoxFit.cover)
         ),
           child: Column(
@@ -46,7 +46,7 @@ class LogInSignIn extends StatelessWidget {
                 constraints: BoxConstraints.tight(Size(300, 300)),
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage("lib/assets/logowithoutbg.png"))
+                    image: NetworkImage('https://media.discordapp.net/attachments/1033448117703561326/1033601816224595978/logowithoutbg.png?width=499&height=499'))
                 ),
               ),
               PhysicalModel(color: Color.fromRGBO(228, 205, 205, 0.75),
@@ -98,25 +98,27 @@ class LogInSignIn extends StatelessWidget {
                 children: [
                   Container(
                     constraints: BoxConstraints.tight(
-                      Size(90, 90)
+                      Size(150, 150)
                     ),
+                    margin: EdgeInsets.all(20),
                     decoration: BoxDecoration(
                       color: Colors.transparent,
                       image: DecorationImage(
-                        image: AssetImage("lib/assets/tesflogo.png"),
-                        fit: BoxFit.cover
+                        image: NetworkImage('https://media.discordapp.net/attachments/1033448117703561326/1033633180453961799/1630133662558_1.png?width=565&height=499'),
+                        fit: BoxFit.fill
                     ),
                   ),
                   ),
                   Container(
                     constraints: BoxConstraints.tight(
-                      Size(90, 90)
+                      Size(150, 150)
                     ),
+                    margin: EdgeInsets.all(20),
                     decoration: BoxDecoration(
                       color: Colors.transparent,
                       image: DecorationImage(
-                        image: AssetImage("lib/assets/zlwbg.png"),
-                        fit: BoxFit.cover)
+                        image: NetworkImage('https://media.discordapp.net/attachments/1033448117703561326/1033633396443848714/zenith_logo_without_bg.png'),
+                        fit: BoxFit.fill)
                     ),
                   ),
                 ],
