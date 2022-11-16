22202b765aaedcfe26c6cdd6d0aa74a32fcd9f8d
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-11-17 00:38:53 +0530

    major changes

--- daybreakglimmer/lib/main.dart
+++ daybreakglimmer/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, must_call_super, must_be_immutable
+// ignore_for_file: prefer_const_constructors, must_call_super, must_be_immutable, avoid_print
 
 import 'dart:async';
 import 'package:flutter/material.dart';
@@ -10,6 +10,9 @@ void main() => {
           SystemUiOverlayStyle(statusBarColor: Colors.transparent)),
       runApp(EarthSaviours())
     };
+void _homepage(BuildContext context) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => Homepage()));
+}
 
 class EarthSaviours extends StatelessWidget {
   const EarthSaviours({Key? key}) : super(key: key);
@@ -22,7 +25,21 @@ class EarthSaviours extends StatelessWidget {
     );
   }
 }
-
+void loginalert(BuildContext context) {
+  final alert = AlertDialog(
+    title: const Text('Login Failed'),
+    content: const Text('Username or Password was incorrect'),
+    actions: [
+      TextButton(onPressed: () => Navigator.pop(context), child: const Text('Retry'),)
+    ],
+  );
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
 class _Splashscreen extends StatefulWidget {
   const _Splashscreen({Key? key}) : super(key: key);
 
@@ -220,56 +237,11 @@ class LogInSignIn extends StatelessWidget {
   }
 }
 
-
 //? Home Page
-void _homepage(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title:
-            Text('Home', style: TextStyle(fontFamily: 'Arial Rounded MT Bold')),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(228, 205, 205, 0.75),
-        foregroundColor: Colors.black,
-        shadowColor: Colors.black,
-        elevation: 20,
-      ),
-      body: Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(
-                  'https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
-              fit: BoxFit.cover),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Color.fromRGBO(161, 91, 91, 0.75),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            IconButton(
-              onPressed: null,
-              icon: Icon(Icons.warning_rounded),
-              tooltip: 'Notifications',
-              iconSize: 36,
-            ),
-            IconButton(
-              onPressed: null,
-              icon: Icon(Icons.home_filled),
-              tooltip: 'Home',
-              iconSize: 36,
-            ),
-            IconButton(
-              onPressed: null,
-              icon: Icon(Icons.person),
-              tooltip: 'Profile',
-              iconSize: 36,
-            ),
-          ],
-        ),
-      ),
-    );
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
 
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -318,20 +290,25 @@ void _homepage(BuildContext context) => Scaffold(
       ),
     );
   }
+}
 
-//* Login page starts from here.
+//* Login page starts from here. Point to be noted here is 
+//* once the user enters password for login, user will be checked if it is admin or not, 
+//* If the user is an admin and the password given is correct, then AdminHomePage will load
+//* Else the user is a normal user and the password given is correct, then HomePage will load.
+
 class Login extends StatelessWidget {
-   Login({Key? key}) : super(key: key);
+  Login({Key? key}) : super(key: key);
 
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  
+  TextEditingController loginuserController = TextEditingController();
+  TextEditingController loginpassController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'T.E.S.F',
+          'T.E.S.F Login',
           style: TextStyle(fontFamily: 'Arial Rounded MT Bold'),
         ),
         centerTitle: true,
@@ -341,46 +318,107 @@ class Login extends StatelessWidget {
         elevation: 20,
       ),
       body: Container(
+        alignment: Alignment.center,
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: NetworkImage(
                     'https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
                 fit: BoxFit.cover)),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
+        child: PhysicalModel(
+          color: Colors.transparent,
+          shadowColor: Colors.black,
+          elevation: 20,
+          child: Container(
+            width: 350,
+            height: 300,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(228, 205, 205, 0.75),
+                borderRadius: BorderRadius.circular(15)),
+            child: ListView(children: [
               Container(
-                width: 350,
-                height: 900,
+                alignment: Alignment.center,
+                width: 200,
+                height: 50,
+                margin: EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                  color: Color.fromRGBO(228, 205, 205, 0.75),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                        if (states.contains(MaterialState.pressed)) {
-                          return Color.fromRGBO(228, 205, 205, 0.75);
-                        }
-                        return Color.fromRGBO(
-                            161, 91, 91, 0.75); // Use the component's default.
-                      }, //161, 91, 91, 0.75
-                    ),
-                  ),
-                  onPressed: () {
-                    
-                  },
-                  child: Text(
-                    '->',
-                    style: TextStyle(
-                      fontFamily: 'Arial Rounded MT Bold',
-                    ),
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color.fromRGBO(161, 91, 91, 0.75)),
+                child: Text(
+                  'Login',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w100,
+                    fontSize: 24,
+                    fontFamily: 'Arial Rounded MT Bold',
+                    color: Colors.black,
                   ),
                 ),
               ),
-            ],
+              Container(
+                  margin: EdgeInsets.all(5),
+                  child: TextFormField(
+                    controller: loginuserController,
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 0.0)),
+                      labelText: 'Username',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  child: TextFormField(
+                    controller: loginpassController,
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 0.0)),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () async {
+                      var url =
+                          Uri.parse('https://daybreaklimit.herokuapp.com/auth/login');
+                      var response = await http.post(url, body: {
+                        'user[username]': loginuserController.text,
+                        'user[password]':loginpassController.text,
+                      });
+                      if (response.statusCode == 200) {
+                        print(response.body);
+                        return _homepage(context);
+                      }
+                      if (response.statusCode == 401) {
+                        print(response.body);
+                        return loginalert(context);
+                      }
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 200,
+                      height: 50,
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromRGBO(161, 91, 91, 0.75),
+                      ),
+                      child: Text(
+                        'Proceed',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Arial Rounded MT Bold',
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ))
+                
+            ]),
           ),
         ),
       ),
@@ -431,29 +469,28 @@ class SignIn extends StatelessWidget {
             width: 350,
             height: 600,
             decoration: BoxDecoration(
-              color: Color.fromRGBO(228, 205, 205, 0.75),
-              borderRadius: BorderRadius.circular(15)
-            ),
+                color: Color.fromRGBO(228, 205, 205, 0.75),
+                borderRadius: BorderRadius.circular(15)),
             child: ListView(
               children: [
                 Container(
                   alignment: Alignment.center,
                   width: 200,
                   height: 50,
-                  margin:  EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color.fromRGBO(161, 91, 91, 0.75)
-                    
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromRGBO(161, 91, 91, 0.75)),
+                  child: Text(
+                    'Sign Up',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w100,
+                      fontSize: 24,
+                      fontFamily: 'Arial Rounded MT Bold',
+                      color: Colors.black,
+                    ),
                   ),
-                  child: Text('Sign Up', 
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w100,
-                    fontSize: 24,
-                    fontFamily: 'Arial Rounded MT Bold',
-                    color: Colors.black,
-                  ),),
                 ),
                 Container(
                   margin: EdgeInsets.all(5),
@@ -533,39 +570,42 @@ class SignIn extends StatelessWidget {
                     ),
                   ),
                 ),
-                TextButton(onPressed: () async {
-                  var url = Uri.parse('https://daybreaklimit.herokuapp.com/user');
-                  var response = await http.post(url, body: {
-
-                  'user[first_name]': fnameController.text,
-                  'user[last_name]': lnameController.text,
-                  'user[username]': usernameController.text,
-                  'user[password]' : passwordController.text,
-                  'user[mail]' : emailController.text,
-                  'user[phone]' : contactnumberController.text,
-              });
-                  if (response.statusCode == 200) {
-                    return _homepage(context);
-                  }
-                },
-                 
-                 child: Container(
-                  alignment: Alignment.center,
-                  width: 200,
-                  height: 50,
-                  margin:  EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color.fromRGBO(161, 91, 91, 0.75),               
-                  ),
-                  child: Text('Proceed', 
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Arial Rounded MT Bold',
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),),
-                ))
+                TextButton(
+                    onPressed: () async {
+                      var url =
+                          Uri.parse('https://daybreaklimit.herokuapp.com/user');
+                      var response = await http.post(url, body: {
+                        'user[first_name]': fnameController.text,
+                        'user[last_name]': lnameController.text,
+                        'user[username]': usernameController.text,
+                        'user[password]': passwordController.text,
+                        'user[mail]': emailController.text,
+                        'user[phone]': contactnumberController.text,
+                      });
+                      if (response.statusCode == 200) {
+                        print(response.body);
+                        return _homepage(context);
+                      }
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 200,
+                      height: 50,
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromRGBO(161, 91, 91, 0.75),
+                      ),
+                      child: Text(
+                        'Proceed',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Arial Rounded MT Bold',
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ))
               ],
             ),
           ),
@@ -577,7 +617,6 @@ class SignIn extends StatelessWidget {
     );
   }
 }
-
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -602,6 +641,10 @@ class Profile extends StatelessWidget {
                   'https://cdn.discordapp.com/attachments/1033448117703561326/1033448248385478738/AppBackground.png'),
               fit: BoxFit.cover),
         ),
+
+        //* Profile page body of the person viewing the app.
+
+        child: Column(),
       ),
       bottomNavigationBar: BottomAppBar(
         color: Color.fromRGBO(161, 91, 91, 0.75),
